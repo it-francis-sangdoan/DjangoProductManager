c91889a3eac5dfefb238c715e2b5041d080a6c7a
francis <138024292+FrancisThanhSang@users.noreply.github.com>
2024-05-14 16:25:07 +0700

    chore(settings.py): import os module to use in settings file feat(settings.py): add 'products' app to INSTALLED_APPS to include it in the project feat(settings.py): add TEMPLATES_DIR and STATIC_DIR variables to specify the directories for templates and static files feat(settings.py): add TEMPLATES_DIR to DIRS list in TEMPLATES settings to include the templates directory in the project feat(settings.py): add STATIC_DIR to STATICFILES_DIRS list to include the static directory in the project feat(products/urls.py): create new file for product management app's URLs

--- product_management/product_management/settings.py
+++ product_management/product_management/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -51,10 +52,13 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'product_management.urls'
 
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [STATIC_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
